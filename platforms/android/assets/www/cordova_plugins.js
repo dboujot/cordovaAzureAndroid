f854c9f198c659bab0df31945ac8722cb9daae2f
David BOUJOT <david.boujot@netapsys.fr>
2015-04-17 17:45:40 +0200

    ajout du plugin de push

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -10,12 +10,20 @@ module.exports = [
         "clobbers": [
             "WindowsAzure.Messaging.NotificationHub"
         ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "msopentech.azure.NotificationHub": "0.0.1"
+    "msopentech.azure.NotificationHub": "0.0.1",
+    "com.phonegap.plugins.PushPlugin": "2.4.0"
 }
 // BOTTOM OF METADATA
 });
